05f75efe492698e26725d11045580d988046b626
RISHABH B <itsrishabhbisht@gmail.com>
2025-05-29 02:14:59 +0530

    Update ddl.bronze.sql

--- scripts/bronze/ddl.bronze.sql
+++ scripts/bronze/ddl.bronze.sql
@@ -43,7 +43,7 @@ IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_sales_details;
 CREATE TABLE bronze.crm_sales_details (
     sls_ord_num VARCHAR(50),
-    sls_prd_key VARCHAR(50),
+    sls_prd_key INT,
     sls_cust_id INT,
     sls_order_dt VARCHAR(50),
     sls_ship_dt VARCHAR(50),
